768308b21cdd23b18a29ca7f1dddc94f5b9e3a81
Jorge Murria <jmurriah@gmail.com>
2024-12-24 06:21:07 +0100

    Adding the server.py

---
+++ server.py
@@ -0,0 +1,20 @@
+# Import the Flask class from the flask module
+from flask import Flask
+from EmotionDetection.emotion_detection import emotion_detector
+
+# Create an instance of the Flask class, passing in the name of the current module
+app = Flask("EmotionDetection")
+
+@app.route("/emotionDetector/<string:emotion>")
+def emotion_detection(emotion):
+    # Function that handles requests to the root URL
+    emotions = emotion_detector(emotion)
+    
+    res = "For the give statement, the system response is "
+    for  key in emotions:
+        if (key != "dominant_emotion"):
+            res += " '" + key + "': " + str(emotions[key]) + ", "
+    res = res[:-2]
+    res += ". The dominant emotion is <b>" + emotions["dominant_emotion"] + "</b>."
+
+    return res
