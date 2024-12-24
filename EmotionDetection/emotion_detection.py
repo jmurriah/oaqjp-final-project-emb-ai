993bcc6b2636e75692471fb7428ed9faecbe2b0e
Jorge Murria <jmurriah@gmail.com>
2024-12-24 06:22:08 +0100

    Managing errors

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -6,6 +6,10 @@ def emotion_detector(text_to_analyze):
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     json_data = { "raw_document": { "text": text_to_analyze } }
     response = requests.post(url, json = json_data, headers = header)  # Send a POST request to the API with the text and headers
+
+    if (response.status_code == 400):
+        return {'anger': None, 'disgust': None, 'fear': None, 'joy': None, 'sadness': None , 'dominant_emotion': None }
+
     formatted = json.loads(response.text)
     emotions = formatted['emotionPredictions'][0]['emotion']
     #print(emotions)
@@ -14,3 +18,4 @@ def emotion_detector(text_to_analyze):
     emotions.update( {"dominant_emotion" : key_value_max})
     #print(emotions)
     return emotions
+
--- server.py
+++ server.py
@@ -5,11 +5,15 @@ from EmotionDetection.emotion_detection import emotion_detector
 # Create an instance of the Flask class, passing in the name of the current module
 app = Flask("EmotionDetection")
 
+@app.route("/emotionDetector/")
 @app.route("/emotionDetector/<string:emotion>")
-def emotion_detection(emotion):
+def emotion_detection(emotion = None):
     # Function that handles requests to the root URL
     emotions = emotion_detector(emotion)
     
+    if emotions["dominant_emotion"] == None:
+        return "Invalid text! Please try again!"
+
     res = "For the give statement, the system response is "
     for  key in emotions:
         if (key != "dominant_emotion"):
